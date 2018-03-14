a45741baf7ff8c25cfb09d6918eecf940d98f24a
Sam Thomson <samt@samt.st>
2018-03-14 13:19:22 +0700

    model refacotr

--- src/db/models.ts
+++ src/db/models.ts
@@ -4,6 +4,11 @@ import database from './db'
 export const LogEntry = database.define(
     'log_entry',
     {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      }
     },
     {
       timestamps: true,
@@ -15,9 +20,14 @@ export const LogEntry = database.define(
 export const Currency = database.define(
     'currency',
     {
-      currency_literal_id: Sequelize.STRING,
-      name: Sequelize.STRING,
-      symbol: Sequelize.STRING,
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      currency_literal_id: Sequelize.STRING(128),
+      name: Sequelize.STRING(128),
+      symbol: Sequelize.STRING(32),
     },
     {
       timestamps: true,
@@ -28,23 +38,27 @@ export const Currency = database.define(
 export const CurrencyEntry = database.define(
     'currency_entry',
     {
-      currency: Sequelize.INTEGER,
-      log_entry: Sequelize.INTEGER,
-      rank: Sequelize.INTEGER,
-      price_usd: Sequelize.DECIMAL(32,24),
-      price_btc: Sequelize.DECIMAL(32,24),
-      volume_usd_24h: Sequelize.DECIMAL(32,4),
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      currency: Sequelize.SMALLINT.UNSIGNED,
+      log_entry: Sequelize.INTEGER.UNSIGNED,
+      rank: Sequelize.SMALLINT.UNSIGNED,
+      price_usd: Sequelize.DECIMAL(32,12),
+      price_btc: Sequelize.DECIMAL(32,12),
+      volume_usd_24h: Sequelize.DECIMAL(32,2),
       market_cap_usd: Sequelize.DECIMAL(32,4),
       available_supply: Sequelize.DECIMAL(32,4),
       total_supply: Sequelize.DECIMAL(32,4),
       percent_change_1h: Sequelize.DECIMAL(8,4),
       percent_change_24h: Sequelize.DECIMAL(8,4),
       percent_change_7d: Sequelize.DECIMAL(8,4),
-      last_updated: Sequelize.INTEGER
+      last_updated: Sequelize.INTEGER,
     },
     {
-      timestamps: true,
-      updatedAt: false,
+      timestamps: false,
       freezeTableName: true,
     }
 )
