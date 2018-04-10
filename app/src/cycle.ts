b74b52b73e0284258463b189e59a3bf22dabeac6
Sam Thomson <samt@samt.st>
2018-04-11 00:05:17 +0100

    started

--- app/src/cycle.ts
+++ app/src/cycle.ts
@@ -6,4 +6,4 @@ import { CronJob } from 'cron'
 var taskEveryMinute = new CronJob('* * * * *', function() {
     // run every minute
     main()
-}, () => {}, true, 'Europe/London');
+}, () => {}, true, 'Europe/fdfdsfss');
